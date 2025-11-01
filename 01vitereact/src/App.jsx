786afaa05c27c4dc8c4382e99120136dfa52dbb2
Satrohan sharma <jerrypark611@gmail.com>
2025-11-01 20:29:26 +0545

    done

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+ <h1>Code with Satrohan sharma from kantibazar and i fully focused in my coding journey</h1>
+  )
+}
+
+export default App
---
+++ first.js
@@ -0,0 +1,1 @@
+console.log("hello buddy its me satrohan sharma and i'm from kantibazar and i have willingness to learn new things in my life");
